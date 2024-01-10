f99bffb407bd8fbfb3f51f48fb18733cf0c1d850
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-01-10 20:29:59 +0530

    navbar

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./TopBar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+      <div className='topLeft'>
+      <i className="topIcon fa-brands fa-facebook"></i>
+      <i className="topIcon fa-brands fa-twitter"></i>
+      <i className="topIcon fa-brands fa-linkedin"></i>
+      <i className="topIcon fa-brands fa-instagram"></i>
+      </div>
+      <div className='topCenter'>
+        <ul className='topList'>
+            <li className='topListItem'>
+                HOME
+            </li>
+            <li className='topListItem'>
+                ABOUT
+            </li>
+            <li className='topListItem'>
+                CONTACT
+            </li>
+            <li className='topListItem'>
+                WRITE
+            </li>
+            <li className='topListItem'>
+                LOGOUT
+            </li>
+
+        </ul>
+      </div>
+      <div className='topRight'>
+        <i class="searchIcon fa-solid fa-user"></i>
+        <i class="searchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  )
+}
