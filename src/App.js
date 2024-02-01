be27ac2e7d6f73b344f49fff7d19f7c3c3dc5822
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-02-01 23:48:11 +0530

    updated react file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import TopBar from "./topbar/TopBar";
-
+import Home from "./home/Home";
+import Header from "./header/Header";
+import SideBar from "./sidebar/SideBar";
+import Posts from "./posts/Posts"
 function App() {
   return (
     <>
     <TopBar/>
-    <p></p>
+    
+    <Home/>
     </>
   );
 }
